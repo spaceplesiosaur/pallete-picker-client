f5946f40938da33b2205ba3ce58606666e9d85fa
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-02-07 16:10:58 -0700

    Add layout for nav bar

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Nav.scss';
+
+const Nav = () => {
+    return (
+        <nav>
+           <h1>Palette Picker</h1>
+        </nav>
+    );
+};
+
+export default Nav;
