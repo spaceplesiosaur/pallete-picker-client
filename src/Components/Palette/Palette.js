a633cf06553df61a0a3bd6276554a563a21d0c6c
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-02-09 10:39:29 -0700

    Add basic layout to Palette container and add react icons package to add locks for palettes

---
+++ src/Components/Palette/Palette.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Palette.scss';
+import { FiLock, FiUnlock } from 'react-icons/fi';
+
+const Palette = () => {
+    return (
+      <section>
+
+        <FiLock />
+      </section>
+    )
+};
+
+export default Palette;
