97d3858ba5dd4159ecfbcd97cc7a817bcfdd7005
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-02-09 11:01:07 -0700

    Add color, lock and hex name to page

--- src/Components/Palette/Palette.js
+++ src/Components/Palette/Palette.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './Palette.scss';
 import { FiLock, FiUnlock } from 'react-icons/fi';
 
-const Palette = () => {
+const Palette = ({ color1, color2, color3, color4, color5 }) => {
+    console.log(color1)
     return (
-      <section>
-
+      <section style={{backgroundColor:`${color1}`}} >
+       
         <FiLock />
+        <p>HexCode: #{color1}</p>
       </section>
     )
 };
