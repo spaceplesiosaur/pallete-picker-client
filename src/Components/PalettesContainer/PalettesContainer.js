7ca829a4cb36b2ca7715b2e5ed8bd0550d1bdeee
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-02-09 10:13:38 -0700

    Add basic layout for PalettesContainer

---
+++ src/Components/PalettesContainer/PalettesContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PalettesContainer.scss';
+
+const PalettesContainer = () => {
+    return (
+      <section>
+
+      </section>
+    );
+}
+
+export default PalettesContainer;
