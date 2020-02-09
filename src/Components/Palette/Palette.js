467c3ba483612da17c8bad6247529f3fc802d233
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-02-09 11:09:08 -0700

    Fix syntax error in palette classname

--- src/Components/Palette/Palette.js
+++ src/Components/Palette/Palette.js
@@ -5,10 +5,10 @@ import { FiLock, FiUnlock } from 'react-icons/fi';
 const Palette = ({ color1, color2, color3, color4, color5 }) => {
     console.log(color1)
     return (
-      <section clasName='palette' style={{backgroundColor:`${color1}`}} >
+      <section className='palette' style={{backgroundColor:`${color1}`}} >
        
         <FiLock />
-        <p>HexCode: #{color1}</p>
+        <p>HexCode: {color1}</p>
       </section>
     )
 };
