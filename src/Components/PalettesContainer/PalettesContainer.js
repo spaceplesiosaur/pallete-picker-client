5642fb3bd634c8de650f3e99e0eb9fe6f62fc20c
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-02-09 10:29:06 -0700

    Add useReducer and mock palettes to get some palettes on the page

--- src/Components/PalettesContainer/PalettesContainer.js
+++ src/Components/PalettesContainer/PalettesContainer.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import './PalettesContainer.scss';
 
+const paletteReducer = (state, action) => {
+  switch(action.type) {
+      case 'palettes':
+        return action.palettes;
+      case 'add':
+        return [...state, {
+            name: action.name,
+            color1: action.color1,
+            color2: action.color2,
+            color3: action.color3,
+            color4: action.color4,
+            color5: action.color5
+          }]
+     default:
+        return state;
+  }
+};
+
 const PalettesContainer = () => {
+
+   const [state, dispatch] = useReducer(paletteReducer, []);
+   const allPalettes = [{
+    name: "Ocean",
+    color1: "#DFE9FD",
+    color2: "#053BA7",
+    color3: "#B3C0F7",
+    color4: "#4B93FB",
+    color5: "#00A8CF"
+  }]
+
     return (
       <section>
 
