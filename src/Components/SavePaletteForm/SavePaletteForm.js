b86f12b0b67ddbf0ad55f5c93a0bb0b9b051a526
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-02-09 11:12:59 -0700

    Add basic layout to savePaletteForm

---
+++ src/Components/SavePaletteForm/SavePaletteForm.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SavePaletteForm.scss';
+
+const SavePaletteForm = () => {
+    return (
+       <form className='palette-form'>
+           <select></select>
+           <input />
+           <button>Save Palette</button>
+       </form>
+    )
+}
+
+export default SavePaletteForm;
