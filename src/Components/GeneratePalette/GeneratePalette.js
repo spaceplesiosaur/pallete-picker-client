6cd5f36643c7838ea8efb80ed9860a2d858f9f6e
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-02-09 11:16:48 -0700

    Add basic layout to GeneratePalette container

---
+++ src/Components/GeneratePalette/GeneratePalette.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './GeneratePalette.scss';
+
+const GeneratePalette = () => {
+    return ( 
+      <button> Generate Palette !</button>
+    )
+}
+
+export default GeneratePalette;
