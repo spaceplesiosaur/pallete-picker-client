275101cdd5b01d7335a5b1508f531a4b33fe61ea
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-02-10 20:28:09 -0700

    Refactor random method

--- src/Components/PalettesContainer/PalettesContainer.js
+++ src/Components/PalettesContainer/PalettesContainer.js
@@ -7,30 +7,28 @@ import GeneratePalette from '../GeneratePalette/GeneratePalette';
 
 const PalettesContainer = () => {
 
-  const colorListState = useState([{name: "color1", color: "#000000", frozen: false}, {name: "color2", color: "#ffffff", frozen: false}, {name: "color3", color: "#000000", frozen: false}, {name: "color4", color: "#ffffff", frozen: false}, {name: "color5", color: "#000000", frozen: false} ])
-  const colorList = colorListState[0]
-  const changeColor = colorListState[1]
+  const colorListState = useState([{name: "color1", color: "#000000", frozen: false}, {name: "color2", color: "#ffffff", frozen: false}, {name: "color3", color: "#000000", frozen: false}, {name: "color4", color: "#ffffff", frozen: false}, {name: "color5", color: "#000000", frozen: false} ]);
+  const colorList = colorListState[0];
+  const changeColor = colorListState[1];
 
   const freezeColor = (name) => {
     const updatedColors = colorList.forEach(color => {
       if (color.name === name) {
         color.frozen = !color.frozen
       }
-    })
+    });
     changeColor(updatedColors)
   }
 
   const randomizeColors = () => {
     const newColors = colorList.map(palette => {
       if (palette.frozen === false) {
-        palette.color = `#${Math.floor(Math.random()*16777215).toString(16)}`
-      } else {
-        palette.color = palette.color
-      }
+        palette.color = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+      } 
       return palette;
-    })
+    });
 
-     changeColor(newColors)
+     changeColor(newColors);
   }
 
   const makeColors = colorList.map((palette) => {
