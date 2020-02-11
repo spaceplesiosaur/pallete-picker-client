acc767e31cf307857150c515c75af2abe78ffec6
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-02-11 13:43:42 -0700

    Add hooks to paletteForm

--- src/Components/SavePaletteForm/SavePaletteForm.js
+++ src/Components/SavePaletteForm/SavePaletteForm.js
@@ -1,14 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SavePaletteForm.scss';
 
 const SavePaletteForm = () => {
+
+   const [paletteName, setPaletteName] = useState('');
+   
+    // build a method that posts the palette on click of saved palette
+
+
+    // if input is empty, disable button
+
+    // var that maps through the projects and add inside select
+        // projects.map --> <option> project.name </option>
+
+    const handleSubmit = (e) => {
+       e.preventDefault();
+
+       setPaletteName('');
+    }
+
     return (
        <form className='palette-form'>
-           <select></select>
-           <input />
-           <button>Save Palette</button>
+           <select>
+
+           </select>
+           <input type='text' 
+                  value={paletteName} 
+                  onChange={(e) => setPaletteName(e.target.value)} 
+            />
+           <button onClick={handleSubmit}>Save Palette</button>
        </form>
-    )
-}
+    ) 
+};
 
 export default SavePaletteForm;
