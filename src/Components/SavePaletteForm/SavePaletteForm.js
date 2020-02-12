15aa15fa0a97f4e5a668fa029aa5fde0e6b67d39
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-02-11 23:08:00 -0700

    Fix bug in SavePAletteForm, didnt have functionality from recent branch

--- src/Components/SavePaletteForm/SavePaletteForm.js
+++ src/Components/SavePaletteForm/SavePaletteForm.js
@@ -1,35 +1,23 @@
 import React, { useState } from 'react';
 import './SavePaletteForm.scss';
-
 import { postProject } from '../../apiCalls';
 import { connect } from 'react-redux';
 
-const SavePaletteForm = ({ colorList, allSetProjects }) => {
-
+export const SavePaletteForm = ({ colorList, allSetProjects }) => {
    const [paletteName, setPaletteName] = useState('');
-   const [projectID, setProjectID] = useState('')
-    
-    // const projectNames = () => {
-    //    return allSetProjects.map((project) => {
-    //        return <option>{project.name}</option>
-    //    })
-    // };
+   const [projectID, setProjectID] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
        postPalette();
        setPaletteName('');
-       console.log(allSetProjects)
     }
-
     const postPalette = async () => {
         let completeColours = colorList.map((palette) => {
             return palette.color;
         });
-
-
         let palettes = {
-            project_id: 12,
+            project_id: projectID,
             name: paletteName,
             color1: completeColours[0],
             color2: completeColours[1],
@@ -37,37 +25,39 @@ const SavePaletteForm = ({ colorList, allSetProjects }) => {
             color4: completeColours[3],
             color5: completeColours[4]
           };
-    
       await postProject('https://palette-picker-ac.herokuapp.com/api/v1/palettes', palettes, 'palettes');
-    }
+    };
+
+    const displayProjects = allSetProjects.map(project => {
+           return (
+            <option key={project.id} value={project.id}>{project.name}</option>
+           )
+    });
+
+    const chosenProject = (value) => {
+      setProjectID(value)
+    };
 
     const disableBtn = paletteName.length ? false : true;
-    const displayProjects = () => {
-        console.log('HI', allSetProjects)
-       return allSetProjects.map(project => {
-           return (
-            <option value={project.id} onClick={() => {setProjectID(project.id)}}>{project.name}</option>
-           )
-       })
-    }
 
     return (
        <form className='palette-form'>
-           <select>
-              {displayProjects()}
+           <select onChange={(e) => chosenProject(e.target.value)}>
+               <option>Choose Project</option>
+               {displayProjects}
            </select>
-           <input type='text' 
+           <input type='text'
                   placeholder='Add Palette Name'
-                  value={paletteName} 
-                  onChange={(e) => setPaletteName(e.target.value)} 
+                  value={paletteName}
+                  onChange={(e) => setPaletteName(e.target.value)}
             />
            <button disabled={disableBtn} onClick={handleSubmit}>Save Palette</button>
        </form>
-    ) 
+    )
 };
 
 const mapStateToProps = ({allSetProjects}) => ({
     allSetProjects
-})
+});
 
 export default connect(mapStateToProps, null)(SavePaletteForm);
