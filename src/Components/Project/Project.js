7380865edba1f774d7e206c856c6cfebf665d611
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-02-12 19:52:32 -0700

    Add hover effect that shows hex codes

--- src/Components/Project/Project.js
+++ src/Components/Project/Project.js
@@ -15,10 +15,10 @@ const Project = ({ name, palettes, removeProject, id, fetchPalettes }) => {
       const colorList = [palette.color1, palette.color2, palette.color3, palette.color4, palette.color5]
       const generateColors = colorList.map(color => {
         return (
-          <li className="colorList-tile" style={{backgroundColor: `${color}`}} key={Object.key}></li>
+        <li className="colorList-tile" style={{backgroundColor: `${color}`}} key={Object.key}><span>{color}</span></li>
         )
       })
-      return (
+      return ( 
         <section className="project-palettes-palette">
           <h3>{palette.name}</h3>
           <button className="palette-delete-button" onClick={() => removePalette(palette.id)}>Remove</button>
